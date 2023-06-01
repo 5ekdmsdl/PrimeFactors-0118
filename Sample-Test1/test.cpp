e8904517aa346503bca9f23e80e1caf17da24a2e
dawn <5ekdmsdldl@gmail.com>
2023-06-01 16:06:17 +0900

    [Refactoring] unit test refactoring p..

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,60 +3,57 @@
 #include <gtest/gtest.h>
 #include "../BestReview/primeFactor.cpp"
 
-TEST(PrimeFactor, of0)
+class PrimeFactor_Fix : public testing::Test
 {
+public:
 	PrimeFactor pf;
+};
+
+TEST_F(PrimeFactor_Fix, of0)
+{
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, pf.of(0));
 }
 
-TEST(PrimeFactor, of1)
+TEST_F(PrimeFactor_Fix, of1)
 {
-	PrimeFactor pf;
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, pf.of(1));
 }
 
 
-TEST(PrimeFactor, of2)
+TEST_F(PrimeFactor_Fix, of2)
 {
-	PrimeFactor pf;
 	std::vector<int> expected = {2};
 	EXPECT_EQ(expected, pf.of(2));
 }
 
-TEST(PrimeFactor, of3)
+TEST_F(PrimeFactor_Fix, of3)
 {
-	PrimeFactor pf;
-	std::vector<int> expected = {3};
+	std::vector<int> expected = { 3 };
 	EXPECT_EQ(expected, pf.of(3));
 }
 
-TEST(PrimeFactor, of4)
-{
-	PrimeFactor pf;
-	std::vector<int> expected = {2,2};
+TEST_F(PrimeFactor_Fix, of4){
+	std::vector<int> expected = { 2,2};
 	EXPECT_EQ(expected, pf.of(4));
 }
 
-TEST(PrimeFactor, of6)
+TEST_F(PrimeFactor_Fix, of6)
 {
-	PrimeFactor pf;
-	std::vector<int> expected = {2,3};
+	std::vector<int> expected = { 2, 3 };
 	EXPECT_EQ(expected, pf.of(6));
 }
 
 
-TEST(PrimeFactor, of12)
+TEST_F(PrimeFactor_Fix, of12)
 {
-	PrimeFactor pf;
-	std::vector<int> expected = {2,2,3};
+	std::vector<int> expected = { 2,2,3 };
 	EXPECT_EQ(expected, pf.of(12));
 }
 
-TEST(PrimeFactor, of21)
+TEST_F(PrimeFactor_Fix, of21)
 {
-	PrimeFactor pf;
-	std::vector<int> expected = {3,7};
+	std::vector<int> expected = { 3, 7 };
 	EXPECT_EQ(expected, pf.of(21));
 }
