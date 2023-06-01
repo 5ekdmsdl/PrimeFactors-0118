f2a5d02ee38b0749e1c3bf422bb95fe438b89fa3
dawn <5ekdmsdldl@gmail.com>
2023-06-01 15:56:53 +0900

    [feature] of feature p..

--- BestReview/primeFactor.cpp
+++ BestReview/primeFactor.cpp
@@ -4,9 +4,25 @@
 class PrimeFactor
 {
 public:
-	std::vector<int> of(int i)
+	std::vector<int> of(int t)
 	{
-		std::vector<int> result = {};
-		return result;
+		std::vector<int> pri = {};
+		if (t <= 1) return pri;
+
+		while(t > 1)
+		{
+			for (int n = 2; n <= t; n++)
+			{
+				if (t % n == 0)
+				{
+					pri.push_back(n);
+					t = t / n;
+					break;
+				}
+			}
+		}
+
+		
+		return pri;
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,11 +1,62 @@
 #include "pch.h"
 #include <string>
 #include <gtest/gtest.h>
-#include <C:\reviewer\workspace\test6\BestReview\primeFactor.cpp>
+#include "../BestReview/primeFactor.cpp"
+
+TEST(PrimeFactor, of0)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {};
+	EXPECT_EQ(expected, pf.of(0));
+}
 
 TEST(PrimeFactor, of1)
 {
 	PrimeFactor pf;
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, pf.of(1));
+}
+
+
+TEST(PrimeFactor, of2)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {2};
+	EXPECT_EQ(expected, pf.of(2));
+}
+
+TEST(PrimeFactor, of3)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {3};
+	EXPECT_EQ(expected, pf.of(3));
+}
+
+TEST(PrimeFactor, of4)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {2,2};
+	EXPECT_EQ(expected, pf.of(4));
+}
+
+TEST(PrimeFactor, of6)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {2,3};
+	EXPECT_EQ(expected, pf.of(6));
+}
+
+
+TEST(PrimeFactor, of12)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {2,2,3};
+	EXPECT_EQ(expected, pf.of(12));
+}
+
+TEST(PrimeFactor, of21)
+{
+	PrimeFactor pf;
+	std::vector<int> expected = {3,7};
+	EXPECT_EQ(expected, pf.of(21));
 }
